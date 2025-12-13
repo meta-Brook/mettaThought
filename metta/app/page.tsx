b020a9764105b2eede4c42b1d94cbedf5b937c94
unknown <anderscolbrook@gmail.com>
2025-12-12 19:29:53 -0600

    no import from neo4j runQuery in server

--- metta/app/page.tsx
+++ metta/app/page.tsx
@@ -1,18 +1,10 @@
 // app/page.tsx
-import { runQuery } from "@/lib/neo4j2";
+
 import EntryForm from "./components/EntryForm"
-import View from "./components/View"
+
 
 export default async function HomePage() {
-  // Run a simple query to fetch your Argument nodes
-  const records = await runQuery(`
-    MATCH (a:Argument)
-    RETURN a
-    LIMIT 10
-  `);
 
-  // Extract the properties from each record
-  const argumentsList = records.map(record => record.get("a").properties);
 
   
   return (
